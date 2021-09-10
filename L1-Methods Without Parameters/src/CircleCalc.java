64c00c1602181c5f1c2196fe928055ea2beb5c5f
Dinopandab <coronalizbeth567@gmail.com>
2021-09-10 13:04:12 -0400

    lesson 1 and lesson 2 unit 2

--- L1-Methods Without Parameters/src/CircleCalc.java
+++ L1-Methods Without Parameters/src/CircleCalc.java
@@ -1,16 +1,41 @@
+import javax.swing.*;
+import java.text.DecimalFormat;
+
 public class CircleCalc {
 
 
     public static void main(String[] args) {
 
         //call the area method
+        area();
+        circumference();
 
         //call the circumference method
 
     }
 
-    //write a method to calculate a circle's area
+    public static void area(){
+        DecimalFormat round = new DecimalFormat("#.##");
+        double radius = Double.parseDouble(JOptionPane.showInputDialog("What is the radius?"));
 
+       double area = Math.pow(radius,2) * Math.PI;
+
+        JOptionPane.showMessageDialog(null, "Your area is: " + round.format(area));
+
+
+    }
+    //write a method to calculate a circle's area
+    public static void circumference(){
+
+        DecimalFormat round = new DecimalFormat("#.##");
+
+        double radius = Double.parseDouble(JOptionPane.showInputDialog("What is the radius?"));
+
+        double circumference = radius * Math.PI * 2;
+
+        JOptionPane.showMessageDialog(null, "Your circumference is: " + round.format(circumference));
+
+    }
 
     //write a method to calculate a circle's circumference
 
--- L2-Methods with Parameters/src/LemonadeStand.java
+++ L2-Methods with Parameters/src/LemonadeStand.java
@@ -1,4 +1,12 @@
+import javax.swing.*;
+import java.text.DecimalFormat;
+
 public class LemonadeStand {
+
+    public static double price16 = 0.50;
+    public static double price20 = 0.99;
+    public static double tax = 7.5;
+
 
     /*
         You run a very successful Lemonade Stand.
@@ -16,6 +24,31 @@ public class LemonadeStand {
      */
 
     public static void main(String[] args) {
+
+        int smallCupAmount = Integer.parseInt(JOptionPane.showInputDialog("how many 16 oz did you get?"));
+        int bigCupAmount = Integer.parseInt(JOptionPane.showInputDialog("how many 20 oz did you get?"));
+
+        Cost(smallCupAmount,bigCupAmount);
+
+    }
+
+
+    public static void Cost(int smallCupAmount,int bigCupAmount){
+
+        DecimalFormat round = new DecimalFormat("$#,###.00");
+
+        double cost16 = smallCupAmount * price16;
+
+        double cost20 = bigCupAmount * price20;
+
+        double totalCost = (cost16 + cost20) * (1 + tax/100.0);
+
+        String message = "Your order: ";
+        message += "\nnumber of 16 oz: " + smallCupAmount;
+        message+= "\nnumber of 20 oz: " + bigCupAmount;
+        message += "\nYour total is: " + round.format(totalCost);
+
+        JOptionPane.showMessageDialog(null, message);
 
     }
 
